6966694eb2e6775ddcd9c20ba58d13962e31a970
Hooramaleki <huram1380@gmail.com>
2025-09-19 19:03:01 -0400

    degree and speed

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -16,8 +16,9 @@ float x = 500;
 float y = 500;
 float frequency = 1;
 float amplitude = 100;
-Vector2 startOfLine = { 100, 100 };
-Vector2 endOfLine = { 700, 100 };
+float speed = 100;
+float angle = 30;
+
 
 //Changes world state
 void update()
@@ -34,17 +35,29 @@ void draw()
 {
 	BeginDrawing();
 
-	ClearBackground(PURPLE);
+
+	ClearBackground(DARKPURPLE);
 	DrawText("Hoora Maleki 101579782", 10, float(GetScreenHeight() - 30), 20, BLACK);
 
 
 	GuiSliderBar(Rectangle{ 10, 15, 1000, 20 }, "", TextFormat("%.2f", time), &time, 0, 240);
 	DrawText(TextFormat("T: %6.2f", time), GetScreenWidth() - 140, 10, 30, BLACK);
 
-	DrawCircle(x, y, 70, PINK);
-	DrawCircle(500 + cos(time * frequency) * amplitude, 500 + sin(time * frequency) * amplitude, 70, DARKPURPLE);
 
-	DrawLineEx(startOfLine, endOfLine, 10.0f, BLACK);
+	//slider for speed and angle
+	GuiSliderBar(Rectangle{ 10, 100, 200, 100 }, "", TextFormat("%.0f", speed), &speed, -100, 1000);
+	GuiSliderBar(Rectangle{ 10, 200, 200, 100 }, "", TextFormat("%.0f", angle), &angle, -180, 180);
+
+
+	//DrawCircle(x, y, 70, PINK);
+	//DrawCircle(500 + cos(time * frequency) * amplitude, 500 + sin(time * frequency) * amplitude, 70, DARKPURPLE);
+
+
+	//drawing the line
+	Vector2 startPOS = { 200, GetScreenHeight() - 200 };
+	Vector2 velocity = { cos(angle * DEG2RAD) * speed, -sin(angle * DEG2RAD) * speed};
+
+	DrawLineEx(startPOS, startPOS + velocity, 10, BLACK);
 
 	EndDrawing();
 
